253d603139536f32ae43c3b6220c67b625aae030
lucaxgomex <lg.projectdev@gmail.com>
2023-03-30 17:36:16 -0300

    Configuração das Rows

--- disp-moveis-2023/row_column/lib/main.dart
+++ disp-moveis-2023/row_column/lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Centrelize Column and Row'),
+          backgroundColor: Colors.purple[200],
+        ),
+        body: const App(),
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -19,6 +31,75 @@ class MyApp extends StatelessWidget {
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
+} 
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        // Row 1
+        Row(
+          //Creates even space between each item and their parent container
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Container(
+              color: Colors.blue, 
+              height: 200, 
+              width: 200, 
+            ),
+            Container(
+              color: Colors.blue, 
+              height: 200, 
+              width: 200, 
+            ),
+          ],
+        ),
+        //Row 2
+        SizedBox(
+          height: 200,
+          child: Row(
+            //Stretches to vertically fill its parent container
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  color: Colors.green,
+                  height: 200,
+                  width: 200,
+                  ),
+                ),
+              ],
+            )
+          ),
+        // Row 3
+        Row(
+          //Creates even space between each item and their parent container
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Container(
+              color: Colors.blue, 
+              height: 200, 
+              width: 200, 
+            ),
+            Container(
+              color: Colors.blue, 
+              height: 200, 
+              width: 200, 
+            ),
+            Container(
+              color: Colors.blue, 
+              height: 200, 
+              width: 200, 
+            ),
+          ],
+        )
+      ]
+    );
+  }
 }
 
 class MyHomePage extends StatefulWidget {
@@ -31,34 +112,79 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: [
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          // Row 1
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
+            children: <Widget>[
               Container(
-                color: Colors.purple[650],
-                width: 100.0,
-                height: 100.0,
-              ),
+                color: Colors.blue, 
+                height: 40, 
+                width: 40, 
+                child: const Text('1')),
               Container(
-                //margin: const EdgeInsets.all(5.0),
-                color: Colors.green[800],
-                width: 100.0,
-                height: 100.0,
-              ),
+                color: Colors.blue, height: 40, width: 40, child: const Text('2')),
+              Container(
+                color: Colors.blue, height: 40, width: 40, child: const Text('3')),
             ],
           ),
-          Container(),
-        ],
-      ),
-    );
+          // Row 2
+          Row(
+            children: <Widget>[
+              Container(
+                color: Colors.blue, height: 40, width: 40, child: const Text('1')),
+              //Will expand to fill all remaining space
+              Expanded(
+                  child: Container(
+                    color: Colors.green,
+                    height: 40,
+                    width: 40,
+                    child: const Text('2'))),
+              Container(
+                color: Colors.blue, height: 40, width: 40, child: const Text('3')),
+            ],
+          ),
+          //Row 3
+          SizedBox(
+              height: 200,
+              child: Row(
+                //Stretches to vertically fill its parent container
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Container(
+                    color: Colors.blue,
+                    height: 40,
+                    width: 40,
+                    child: const Text('1')),
+                  Expanded(
+                      child: Container(
+                        color: Colors.green,
+                        height: 40,
+                        width: 40,
+                        child: const Text('2'))),
+                  Container(
+                    color: Colors.blue,
+                    height: 40,
+                    width: 40,
+                    child: const Text('3')),
+                ],
+              )),
+          // Row 4
+          Row(
+            //Creates even space between each item and their parent container
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Container(
+                color: Colors.blue, height: 40, width: 40, child: const Text('1')),
+              Container(
+                color: Colors.blue, height: 40, width: 40, child: const Text('1')),
+              Container(
+                color: Colors.blue, height: 40, width: 40, child: const Text('3')),
+            ],
+          )
+        ]);
   }
 }
