d0cbd9ac89d235e2aa9639ff49ed59e93714b0d9
lucaxgomex <lg.projectdev@gmail.com>
2023-03-30 16:46:14 -0300

    Uso de Column e Row em projeto Flutter

---
+++ disp-moveis-2023/row_column/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                color: Colors.purple[650],
+                width: 100.0,
+                height: 100.0,
+              ),
+              Container(
+                //margin: const EdgeInsets.all(5.0),
+                color: Colors.green[800],
+                width: 100.0,
+                height: 100.0,
+              ),
+            ],
+          ),
+          Container(),
+        ],
+      ),
+    );
+  }
+}
