4b54e9683936c3bb888c051fe88d8f1e47a871a5
lucaxgomex <lg.projectdev@gmail.com>
2023-05-18 15:28:58 -0300

    Estilização do body

--- disp-moveis-2023/07/imc_calculator/lib/main.dart
+++ disp-moveis-2023/07/imc_calculator/lib/main.dart
@@ -76,11 +76,11 @@ class _MyHomeState extends State<MyHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: const Color.fromARGB(255, 226, 230, 231),
       appBar: AppBar(
-        title: const Text('IMC Calculator: '),
-        centerTitle: true,
-        backgroundColor: Colors.green,
+        title: const Text('IMC Calculator'),
+        backgroundColor: const Color.fromARGB(255, 19, 159, 201),
+        elevation: 0,
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.refresh),
@@ -90,127 +90,130 @@ class _MyHomeState extends State<MyHome> {
       ),
       body: SingleChildScrollView(
         //padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          //crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            const CircleAvatar(
-              radius: 50,
-              backgroundImage: NetworkImage(
-                scale: 1000.0,
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Z2YCMPrOwqdLwJ0aSRcuUG_pNollSjK6Pw&usqp=CAU',
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(
+                  scale: 1000.0,
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Z2YCMPrOwqdLwJ0aSRcuUG_pNollSjK6Pw&usqp=CAU',
+                ),
               ),
-            ),
-
-            //Input 1
-            Container(
-              width: 300.0,
-              margin: const EdgeInsets.all(10.0),
-              child: TextField(
-                controller: _controllerPeso,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  //label: Text('teste: '),
-                  labelText: 'Peso (Kg): ',
-                  labelStyle: TextStyle(
-                    color: Colors.green,
-                    //fontSize: 25.0,
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 3,
+        
+              //Input 1
+              Container(
+                width: 300.0,
+                margin: const EdgeInsets.all(10.0),
+                child: TextField(
+                  controller: _controllerPeso,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    //label: Text('teste: '),
+                    labelText: 'Peso (Kg): ',
+                    labelStyle: TextStyle(
                       color: Colors.green,
+                      //fontSize: 25.0,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 3,
+                        color: Colors.green,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-
-            // Input 2
-            Container(
-              width: 300.0,
-              margin: const EdgeInsets.all(10.0),
-              child: TextField(
-                controller: _controllerAltura,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  //label: Text('teste: '),
-                  labelText: 'Altura (Cm): ',
-                  labelStyle: TextStyle(
-                    color: Colors.green,
-                    //fontSize: 25.0,
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 3,
+        
+              // Input 2
+              Container(
+                width: 300.0,
+                margin: const EdgeInsets.all(10.0),
+                child: TextField(
+                  controller: _controllerAltura,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    //label: Text('teste: '),
+                    labelText: 'Altura (Cm): ',
+                    labelStyle: TextStyle(
                       color: Colors.green,
+                      //fontSize: 25.0,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 3,
+                        color: Colors.green,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
+        
+              /*
+                Container(
+                  width: 300.0,
+                  margin: const EdgeInsets.all(10.0),
+                  child: TextFormField(
+                    controller: _alcoolValue,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      label: Text('teste: '),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 300.0,
+                  margin: const EdgeInsets.all(10.0),
+                  child: TextFormField(
+                    controller: _gasValue,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      label: Text('Type here how much gasoline you want: '),
+                    ),
+                  ),
+                ),
+                */
 
-            /*
-              Container(
+              // Buttom
+              SizedBox(
                 width: 300.0,
-                margin: const EdgeInsets.all(10.0),
-                child: TextFormField(
-                  controller: _alcoolValue,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    label: Text('teste: '),
+                height: 50.0,
+                child: ElevatedButton(
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.green),
+                    padding: MaterialStatePropertyAll(
+                      EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 15,
+                      ),
+                    ),
+                  ),
+                  //onPressed: _calculate,
+                  onPressed: _calculate,
+                  child: const Text(
+                    "Calcular",
+                    style: TextStyle(
+                      color: Colors.white,
+                      //fontSize: 25.0,
+                    ),
                   ),
                 ),
               ),
-              Container(
-                width: 300.0,
-                margin: const EdgeInsets.all(10.0),
-                child: TextFormField(
-                  controller: _gasValue,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    label: Text('Type here how much gasoline you want: '),
-                  ),
+        
+              /*
+              const Center(
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text('Press the buttom above to check the result'),
                 ),
               ),
               */
-
-            const SizedBox(),
-
-            // Buttom
-            Container(
-              width: 300.0,
-              height: 50.0,
-              child: ElevatedButton(
-                style: const ButtonStyle(
-                  backgroundColor:
-                      MaterialStatePropertyAll<Color>(Colors.green),
-                  padding: MaterialStatePropertyAll(
-                    EdgeInsets.symmetric(
-                      horizontal: 15,
-                      vertical: 15,
-                    ),
-                  ),
-                ),
-                //onPressed: _calculate,
-                onPressed: _calculate,
-                child: const Text(
-                  "Calcular",
-                  style: TextStyle(
-                    color: Colors.white,
-                    //fontSize: 25.0,
-                  ),
-                ),
-              ),
-            ),
-
-            const Center(
-              child: Padding(
-                padding: EdgeInsets.all(20.0),
-                child: Text('Pressione o botão acima para ver o resultado'),
-              ),
-            ),
-          ],
+        
+            ],
+          ),
         ),
       ),
     );
